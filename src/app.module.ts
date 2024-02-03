ac0bb2f655063f4dc8c30378e0bc2f214ea3112a
guigoliver <profguilherme.as@gmail.com>
2024-02-02 21:43:03 -0300

    feat: database config on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssetsModule } from './assets/assets.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Asset } from './assets/entities/asset.entity';
 
 @Module({
-  imports: [AssetsModule],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'sqlite',    
+    database: 'nest',
+    entities: [Asset],
+    synchronize: true,
+    logging: true,
+  }), AssetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
